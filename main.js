10f323fbae89561f1d2bfaad3e1f90a82e9e60d0
WoangHung <hungleo0308@gmail.com>
2022-03-02 14:10:43 +0700

    hungbale

---
+++ main.js
@@ -0,0 +1,11 @@
+var fullName = 'Đinh Quang Hưng';
+var age = 21;
+
+
+console.log('day là một dòng log')
+confirm('Xác nhận bạn đủ tuổi')
+
+var a ='Hung';
+var b = "Dinh";
+var c = a+b;
+console.log(c);
